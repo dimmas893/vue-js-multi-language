5129984328abf4bd5459691536c775f5f85bdd96
sebelumcahahaya <anandadimmas1204@gmail.com>
2023-09-30 16:39:40 +0700

    first commit

---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,13 @@
+import SelectI18n from './SelectI18n.vue'; import SelectI18n from
+'./SelectI18n.vue';
+<template>
+  <SelectI18n></SelectI18n>
+  <p>{{ $t("message") }}</p>
+  <p>{{ $t("morning") }}</p>
+  <p>{{ $t("afternoon") }}</p>
+  <p>{{ $t("night") }}</p>
+</template>
+
+<script setup>
+import SelectI18n from "./SelectI18n.vue";
+</script>
